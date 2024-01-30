3fbfb8b92de1ac8b0b330366cd8014283bbefa83
dior <dior9054@gmail.com>
2024-01-30 19:03:38 +0300

    runnner

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
+const withPWA = require('next-pwa')({
+    dest: 'public'
+})
+
 /** @type {import('next').NextConfig} */
+
 
 const nextConfig = {
     reactStrictMode: false,
 }
 
-module.exports = nextConfig
+module.exports = withPWA({ nextConfig })
+
+
